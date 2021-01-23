af893054e56b096a08ebe66a9f8f73abeb330d75
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-01-23 04:46:22 +0100

    Fix import in selectable-course-block-row.tsx

--- src/main/webapp/app/modules/enrolling/view/selectable-course-block-row.tsx
+++ src/main/webapp/app/modules/enrolling/view/selectable-course-block-row.tsx
@@ -12,92 +12,86 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Collapse from '@material-ui/core/Collapse';
 import TogetherCourseBlockRow from './together-course-block-row';
 import SelectableCourseBlockDetails from '../../../shared/model/domain/dto/selectable-course-block-details';
+import CourseDetails from 'app/shared/model/domain/dto/course-details';
 
 interface ISelectableCourseBlockRowProps {
-    selectableCourseBlock: SelectableCourseBlockDetails,
-    onSelected: (course: CourseDetails) => void
-};
+  selectableCourseBlock: SelectableCourseBlockDetails;
+  onSelected: (course: CourseDetails) => void;
+}
 
 interface ISelectableCourseBlockRowState {
-    isOpen: boolean
-};
+  isOpen: boolean;
+}
 
 class SelectableCourseBlockRow extends Component<ISelectableCourseBlockRowProps, ISelectableCourseBlockRowState> {
-    constructor(props: ISelectableCourseBlockRowProps) {
-        super(props);
+  constructor(props: ISelectableCourseBlockRowProps) {
+    super(props);
 
-        this.state = {
-            isOpen: false
-        };
-    }
+    this.state = {
+      isOpen: false,
+    };
+  }
 
-    setOpen(isOpen: boolean) {
-        this.setState({ isOpen })
-    }
+  setOpen(isOpen: boolean) {
+    this.setState({ isOpen });
+  }
 
-    isEnrolledInAll() {
-        return this.props.selectableCourseBlock.togetherBlocks.some(tb => tb.courses.every(c => c.enrolled));
-    }
+  isEnrolledInAll() {
+    return this.props.selectableCourseBlock.togetherBlocks.some(tb => tb.courses.every(c => c.enrolled));
+  }
 
-    rendedExpandableItem() {
-        const { selectableCourseBlock } = this.props;
-        const { isOpen } = this.state;
+  rendedExpandableItem() {
+    const { selectableCourseBlock } = this.props;
+    const { isOpen } = this.state;
 
-        return (
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar>
-                        {this.isEnrolledInAll() ? <CheckIcon /> : <ClearIcon />}
-                    </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                    primary={selectableCourseBlock.name}
-                    secondary={'Blok wybieralny'}
-                />
-                <ListItemSecondaryAction>
-                    <IconButton aria-label="expand row" size="small" onClick={() => this.setOpen(!isOpen)}>
-                        {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListItem>
-        );
-    }
+    return (
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>{this.isEnrolledInAll() ? <CheckIcon /> : <ClearIcon />}</Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={selectableCourseBlock.name} secondary={'Blok wybieralny'} />
+        <ListItemSecondaryAction>
+          <IconButton aria-label="expand row" size="small" onClick={() => this.setOpen(!isOpen)}>
+            {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
 
-    renderEmbeddedRows() {
-        const { selectableCourseBlock, onSelected } = this.props;
-        return (
-            <>
-                {selectableCourseBlock.togetherBlocks.map(x => 
-                    <TogetherCourseBlockRow key={x.id} togetherCourseBlock={x} onSelected={onSelected} />
-                )}
-            </>
-        );
-    }
+  renderEmbeddedRows() {
+    const { selectableCourseBlock, onSelected } = this.props;
+    return (
+      <>
+        {selectableCourseBlock.togetherBlocks.map(x => (
+          <TogetherCourseBlockRow key={x.id} togetherCourseBlock={x} onSelected={onSelected} />
+        ))}
+      </>
+    );
+  }
 
-    shouldExpand() {
-        return this.props.selectableCourseBlock.togetherBlocks.length > 1;
-    }
+  shouldExpand() {
+    return this.props.selectableCourseBlock.togetherBlocks.length > 1;
+  }
 
-    render() {
-        const { isOpen } = this.state;
+  render() {
+    const { isOpen } = this.state;
 
-        return (
-            <React.Fragment>
-                {this.shouldExpand()
-                ? (
-                    <>
-                        {this.rendedExpandableItem()}
-                        <Collapse in={isOpen} timeout="auto" unmountOnExit style={{paddingLeft: 20}}>
-                            {this.renderEmbeddedRows()}
-                        </Collapse>
-                    </>
-                ) : (
-                    this.renderEmbeddedRows()
-                )
-            }
-            </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        {this.shouldExpand() ? (
+          <>
+            {this.rendedExpandableItem()}
+            <Collapse in={isOpen} timeout="auto" unmountOnExit style={{ paddingLeft: 20 }}>
+              {this.renderEmbeddedRows()}
+            </Collapse>
+          </>
+        ) : (
+          this.renderEmbeddedRows()
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default SelectableCourseBlockRow;
